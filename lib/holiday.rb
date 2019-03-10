02762b2d9c3a19415d62864b5889c302e33ab0d9
Tommi Cox <tommicox@gmail.com>
2019-03-10 13:42:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holidays.each do |holidays, supplies|
-    
+    puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
