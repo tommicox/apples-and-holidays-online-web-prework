14e5009f187c707bb73093da9691297396d34ddd
Tommi Cox <tommicox@gmail.com>
2019-03-10 13:39:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
+  puts #{
 end
 
 def all_holidays_with_bbq(holiday_hash)
